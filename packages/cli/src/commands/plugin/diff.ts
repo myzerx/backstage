f66dd1d7b0f14e06ea9272d576d23bed8ca6959d
Patrik Oldsberg <poldsberg@gmail.com>
2022-02-03 19:03:20 +0100

    cli: no longer diff dev/ or src/
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/cli/src/commands/plugin/diff.ts
+++ packages/cli/src/commands/plugin/diff.ts
@@ -40,13 +40,8 @@ const fileHandlers = [
     handler: handlers.packageJson,
   },
   {
-    // Not all plugins have routes
-    patterns: ['src/routes.ts'],
-    handler: handlers.skip,
-  },
-  {
     // make sure files in 1st level of src/ and dev/ exist
-    patterns: ['.eslintrc.js', /^(src|dev)\/[^/]+$/],
+    patterns: ['.eslintrc.js'],
     handler: handlers.exists,
   },
   {
